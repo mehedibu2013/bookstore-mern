92c4dfaadd97007660ef6dcd4ec6a4217742e0d0
mehedi <mehedibu2013@gmail.com>
2025-06-17 09:28:18 +0600

    Remove config.js from Git tracking

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-"mongodb+srv://admin765:admin@cluster0.sbyfstw.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Cluster0";
-  //'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
